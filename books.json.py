febb9e1144afe294d908becfba28df75932bae8b
bhristov <magnetgroup@live.com>
2020-02-27 22:17:50 +0200

    Initial commit

---
+++ books.json.py
@@ -0,0 +1,310 @@
+{
+    "books": [
+        {
+            "number_of_pages": 849,
+            "price": 13.55,
+            "publish_date": 2011,
+            "subjects": [
+                "Time travel",
+                "Assassination"
+            ],
+            "title": "11/22/63"
+        },
+        {
+            "number_of_pages": 732,
+            "price": 7.99,
+            "publish_date": 1999,
+            "subjects": [
+                "Authors",
+                "Custody of children",
+                "Grandfathers",
+                "Haunted houses",
+                "Novelists",
+                "Trials (Custody of children)",
+                "Widowers",
+                "Widows",
+                "Writer's block"
+            ],
+            "title": "Bag of bones"
+        },
+        {
+            "number_of_pages": 245,
+            "price": 7.99,
+            "publish_date": 1975,
+            "subjects": [
+                "Telekinesis"
+            ],
+            "title": "Carrie"
+        },
+        {
+            "number_of_pages": 503,
+            "price": 8.99,
+            "publish_date": 1983,
+            "subjects": [
+                "Triangles (Interpersonal relations)",
+                "Teenagers",
+                "Automobiles"
+            ],
+            "title": "Christine"
+        },
+        {
+            "number_of_pages": 304,
+            "price": 7.99,
+            "publish_date": 1982,
+            "subjects": [
+                "Dogs",
+                "Rabies"
+            ],
+            "title": "Cujo"
+        },
+        {
+            "number_of_pages": 402,
+            "price": 7.99,
+            "publish_date": 1979,
+            "subjects": [
+                "Psychics"
+            ],
+            "title": "The dead zone"
+        },
+        {
+            "number_of_pages": 372,
+            "price": 12.69,
+            "publish_date": 1993,
+            "subjects": [
+                "Confession",
+                "Intimacy (Psychology)",
+                "Women"
+            ],
+            "title": "Dolores Claiborne"
+        },
+        {
+            "number_of_pages": 480,
+            "price": 13.59,
+            "publish_date": 2003,
+            "subjects": [
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "The Drawing of the Three (The Dark Tower, Book 2)"
+        },
+        {
+            "number_of_pages": 882,
+            "price": 8.99,
+            "publish_date": 2001,
+            "subjects": [
+                "Human-alien encounters"
+            ],
+            "title": "Dreamcatcher"
+        },
+        {
+            "number_of_pages": 769,
+            "price": 8.06,
+            "publish_date": 2008,
+            "subjects": [
+                "Accident victims",
+                "Supernatural"
+            ],
+            "title": "Duma Key"
+        },
+        {
+            "number_of_pages": 380,
+            "price": 12.75,
+            "publish_date": 1988,
+            "subjects": [
+                "Princes",
+                "Dragons",
+                "Wizards"
+            ],
+            "title": "The eyes of the dragon"
+        },
+        {
+            "number_of_pages": 401,
+            "price": 7.57,
+            "publish_date": 1981,
+            "subjects": [
+                "Psychokinesis",
+                "Fire",
+                "Psychic ability"
+            ],
+            "title": "Firestarter"
+        },
+        {
+            "number_of_pages": 264,
+            "price": 7.99,
+            "publish_date": 2000,
+            "subjects": [
+                "Tom Gordon",
+                "Boston Red Sox",
+                "Appalachian Region",
+                "Appalachian Trail",
+                "Boston",
+                "Children of divorced parents",
+                "Mountain life",
+                "Pitchers (Baseball)"
+            ],
+            "title": "The girl who loved Tom Gordon"
+        },
+        {
+            "number_of_pages": 536,
+            "price": 6.6,
+            "publish_date": 1999,
+            "subjects": [
+                "Death row inmates",
+                "Prisons",
+                "Prisoners"
+            ],
+            "title": "The green mile"
+        },
+        {
+            "number_of_pages": 1138,
+            "price": 7.66,
+            "publish_date": 1986,
+            "subjects": [
+                "Good and evil"
+            ],
+            "title": "It"
+        },
+        {
+            "number_of_pages": 310,
+            "price": 4.83,
+            "publish_date": 1987,
+            "subjects": [
+                "Captives",
+                "Fans (Persons)",
+                "Novelists",
+                "Prisoners",
+                "Writers",
+                "abuse",
+                "fear",
+                "isolation",
+                "prisoner",
+                "Nurses"
+            ],
+            "title": "Misery"
+        },
+        {
+            "number_of_pages": 736,
+            "price": 3.5,
+            "publish_date": 1991,
+            "subjects": [
+                "Castle Rock (Me. : Imaginary place)"
+            ],
+            "title": "Needful things"
+        },
+        {
+            "number_of_pages": 562,
+            "price": 4.99,
+            "publish_date": 2001,
+            "subjects": [
+                "Cemeteries",
+                "Death",
+                "Animals",
+                "Country life",
+                "Resurrection"
+            ],
+            "title": "Pet sematary"
+        },
+        {
+            "number_of_pages": 427,
+            "price": 6.6,
+            "publish_date": 1975,
+            "subjects": [
+                "City and town life",
+                "Vampires"
+            ],
+            "title": "'Salem's Lot"
+        },
+        {
+            "number_of_pages": 560,
+            "price": 7.6,
+            "publish_date": 2006,
+            "subjects": [
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "Song of Susannah (The Dark Tower, Book 6)"
+        },
+        {
+            "number_of_pages": 845,
+            "price": 7.95,
+            "publish_date": 2004,
+            "subjects": [
+                "Good and evil",
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "The dark tower (The Dark Tower, Book 7)"
+        },
+        {
+            "number_of_pages": 447,
+            "price": 5.79,
+            "publish_date": 1978,
+            "subjects": [
+                "Hotelkeepers",
+                "Family",
+                "Families"
+            ],
+            "title": "The Shining"
+        },
+        {
+            "number_of_pages": 1141,
+            "price": 10.08,
+            "publish_date": 1991,
+            "subjects": [
+                "Biological warfare",
+                "Research",
+                "Virus diseases",
+                "Good and evil"
+            ],
+            "title": "The stand"
+        },
+        {
+            "number_of_pages": 747,
+            "price": 8.24,
+            "publish_date": 1988,
+            "subjects": [
+                "Supernatural"
+            ],
+            "title": "The Tommyknockers"
+        },
+        {
+            "number_of_pages": 608,
+            "price": 8.99,
+            "publish_date": 2003,
+            "subjects": [
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "The Waste Lands (The Dark Tower, Book 3)"
+        },
+        {
+            "number_of_pages": 672,
+            "price": 3.78,
+            "publish_date": 1997,
+            "subjects": [
+                "Roland (Fictitious character : King)",
+                "First loves",
+                "Heroes and heroines",
+                "Loss (Psychology)",
+                "Supernatural",
+                "Trains",
+                "Friendship"
+            ],
+            "title": "Wizard and glass"
+        },
+        {
+            "number_of_pages": 960,
+            "price": 7.61,
+            "publish_date": 2006,
+            "subjects": [
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "Wolves of the Calla (The Dark Tower, Book 5)"
+        },
+        {
+            "number_of_pages": 336,
+            "price": 4.94,
+            "publish_date": 1976,
+            "subjects": [
+                "Roland (Fictitious character : King)"
+            ],
+            "title": "The Gunslinger (The Dark Tower, Book 1)"
+        }
+    ]
+}
