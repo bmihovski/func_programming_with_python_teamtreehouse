0a834f2621d69d4deddb8fed0d7f9db490cbbeac
bhristov <magnetgroup@live.com>
2020-02-28 13:43:59 +0200

    Chaining example completed

--- stock.py
+++ stock.py
@@ -35,7 +35,7 @@ RAW_BOOKS = get_books('books.json', raw=True)
 # pages_sort = sorted(BOOKS, key=attrgetter('number_of_pages'))
 # print(pub_sort[0]['publish_date'], pub_sort[-1]['publish_date'])
 # print(pages_sort[0].number_of_pages, pages_sort[-1].number_of_pages)
-
+### MAP ###
 def sales_price(book):
     """
         Apply a 20% discount to the book's price
@@ -46,5 +46,51 @@ def sales_price(book):
 
 
 sales_books = map(sales_price, BOOKS)
-#print(BOOKS[0].price)
-#print(list(sales_books)[0].price)
+
+
+# print(BOOKS[0].price)
+# print(list(sales_books)[0].price)
+
+### FILTER ###
+
+
+def is_long_book(book):
+    """
+        Does a book have 600 pages?
+    """
+
+    return book.number_of_pages >= 600
+
+
+long_books = filter(is_long_book, BOOKS)
+
+
+# print(len(BOOKS))
+# print(len(list(long_books)))
+
+
+### CHAINING ###
+
+
+def has_roland(book):
+    return any(["Roland" in subject for subject in book.subjects])
+
+
+def titlecase(book):
+    book = copy(book)
+    book.title = book.title.title()
+    return book
+
+
+#print(list(map(titlecase, filter(has_roland, BOOKS))))
+
+def is_good_deal(book):
+    return book.price <= 5
+
+
+cheap_books = sorted(
+    filter(is_good_deal, map(sales_price, BOOKS)),
+    key=attrgetter('price')
+)
+
+print(cheap_books[0])
