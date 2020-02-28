aea4fc4d2624e07abec9282a2f0e86dcb9504f18
bhristov <magnetgroup@live.com>
2020-02-28 13:16:22 +0200

    Map example completed

--- stock.py
+++ stock.py
@@ -1,5 +1,7 @@
 import json
+from copy import copy
 from operator import attrgetter, itemgetter
+
 
 class Book:
     def __init__(self, **kwargs):
@@ -23,10 +25,26 @@ def get_books(filename, raw=False):
             return data['books']
         return [Book(**book) for book in data['books']]
 
+
 BOOKS = get_books('books.json')
 
 RAW_BOOKS = get_books('books.json', raw=True)
 
+
 # pub_sort = sorted(RAW_BOOKS, key=itemgetter('publish_date'))
-pages_sort = sorted(BOOKS, key=attrgetter('number_of_pages'))
-print(pages_sort[0].number_of_pages, pages_sort[-1].number_of_pages)
+# pages_sort = sorted(BOOKS, key=attrgetter('number_of_pages'))
+# print(pub_sort[0]['publish_date'], pub_sort[-1]['publish_date'])
+# print(pages_sort[0].number_of_pages, pages_sort[-1].number_of_pages)
+
+def sales_price(book):
+    """
+        Apply a 20% discount to the book's price
+    """
+    book = copy(book)
+    book.price = round(book.price - book.price * .2, 2)
+    return book
+
+
+sales_books = map(sales_price, BOOKS)
+#print(BOOKS[0].price)
+#print(list(sales_books)[0].price)
